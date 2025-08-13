b90b31009fc803ae9f5d576471e86fff0109b9ce
Francesco Dammacco <francescodammacco@live.it>
2025-08-13 13:18:04 +0200

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import restart from 'vite-plugin-restart'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [restart({ restart: ['../public/**'] }), react(), tailwindcss()],
+  server: {
+    host: true,
+    open: true,
+  },
+  build: {
+    emptyOutDir: true,
+    sourcemap: true,
+  },
 })
